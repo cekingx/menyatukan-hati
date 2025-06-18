364bd8d9057054038a0f150dbc6d65610ce8454d
Dirga Yasa <cekingx@gmail.com>
2025-06-18 17:15:21 +0800

    feat: update env value

--- src/environment/env.ts
+++ src/environment/env.ts
@@ -1,12 +1,12 @@
 export const Env = {
   groomShortName: 'Gungde Wahyu',
-  groomFullName: 'Ns. Anak Agung Gde Wahyu Sparsayoga',
-  groomFatherName: 'Gungaji Mangku Bawet',
-  groomMotherName: 'Ibuk gungde wahyu',
-  groomFamName: 'Keluarga Gungaji Mangku Bawet',
-  brideShortName: 'Mitha',
-  brideFullName: 'Ns. Mitha Frianca',
-  brideFatherName: 'Bapak Mitha',
-  brideMotherName: 'Ibuk Mitha',
-  brideFamName: 'Keluarga Bapak Mitha'
+  groomFullName: 'Ns. Anak Agung Gde Wahyu Sparsayoga S.Kep.',
+  groomFatherName: 'Anak Agung Gde Bawa Sudirga',
+  groomMotherName: 'Ni Made Purnawati',
+  groomFamName: 'Keluarga Anak Agung Gde Bawa Sudirga',
+  brideShortName: 'Mitha Frianca',
+  brideFullName: 'Ns. Putu Mitha Frianca Wulandewi S.Kep.',
+  brideFatherName: 'Made Alit Arjana',
+  brideMotherName: 'Ni Luh Putu Asmariani',
+  brideFamName: 'Keluarga Made Alit Arjana'
 }
